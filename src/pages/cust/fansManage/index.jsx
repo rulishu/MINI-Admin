2accdaae81aa3020832e71b2f23c2c73e6c04b73
rulishu <1324214367@qq.com>
2023-05-09 08:29:32 +0800

    fix: 粉丝管理页面

---
+++ src/pages/cust/fansManage/index.jsx
@@ -0,0 +1,12 @@
+import Search from './Search/search';
+import Table from './Table/table';
+
+const Page = () => {
+  return (
+    <div>
+      <Search />
+      <Table />
+    </div>
+  );
+};
+export default Page;
