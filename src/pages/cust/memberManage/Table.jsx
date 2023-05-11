85c5d70eb68cd9aa3fb0b2c7ec1411488693196e
rulishu <1324214367@qq.com>
2023-05-11 13:44:00 +0800

    fix: 会员管理调用分页查询和详情接口

--- src/pages/cust/memberManage/Table.jsx
+++ src/pages/cust/memberManage/Table.jsx
@@ -1,16 +1,14 @@
 import { selectPage } from '@/service/memberManage';
 import { ProTable } from '@ant-design/pro-components';
-import { ButtonGroupPro } from '@antdp/antdp-ui';
-// import { useModel } from '@umijs/max';
+import { useModel } from '@umijs/max';
 import { columns } from './columns';
 
 export default function SearchTable() {
-  // const [pageSize, setPageSize] = useState(10);
-  // const { store, setStore } = useModel('memberManage', (model) => ({ ...model }));
+  const { update } = useModel('memberManage', (model) => ({ ...model }));
 
-  const handle = (type) => {
-    if (type === 'add') {
-      // setStore({ addVisible: true });
+  const handleEdit = (type, data) => {
+    if (type === 'view') {
+      update({ visible: true, queryData: data });
     }
   };
   return (
@@ -35,32 +33,32 @@ export default function SearchTable() {
         showSizeChanger: true,
       }}
       cardBordered
-      columns={columns}
+      columns={columns(handleEdit)}
       rowKey="id"
       search={{
         defaultCollapsed: false,
       }}
-      toolBarRender={() => (
-        <ButtonGroupPro
-          button={[
-            {
-              type: 'primary',
-              label: '创建导出任务',
-              onClick: () => handle('add'),
-            },
-            {
-              type: 'primary',
-              label: '查看导出列表',
-              onClick: () => handle('view'),
-            },
-            {
-              type: 'primary',
-              label: '查看优惠卷发放记录',
-              onClick: () => handle('record'),
-            },
-          ]}
-        />
-      )}
+      // toolBarRender={() => (
+      //   <ButtonGroupPro
+      //     button={[
+      //       {
+      //         type: 'primary',
+      //         label: '创建导出任务',
+      //         onClick: () => handle('add'),
+      //       },
+      //       {
+      //         type: 'primary',
+      //         label: '查看导出列表',
+      //         onClick: () => handle('view'),
+      //       },
+      //       {
+      //         type: 'primary',
+      //         label: '查看优惠卷发放记录',
+      //         onClick: () => handle('record'),
+      //       },
+      //     ]}
+      //   />
+      // )}
     />
   );
 }
