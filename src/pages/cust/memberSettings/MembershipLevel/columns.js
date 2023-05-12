ba3227eaea358935247b5261bb455a88e585cf44
rulishu <1324214367@qq.com>
2023-05-12 14:07:10 +0800

    fix: 调用会员等级分页和新增的接口

--- src/pages/cust/memberSettings/MembershipLevel/columns.js
+++ src/pages/cust/memberSettings/MembershipLevel/columns.js
@@ -1,4 +1,4 @@
-export const columns = (onEdit) => [
+export const columns = () => [
   {
     title: '等级',
     dataIndex: 'level',
@@ -8,14 +8,14 @@ export const columns = (onEdit) => [
   },
   {
     title: '称谓',
-    dataIndex: 'id',
+    dataIndex: 'createName',
     align: 'center',
     hideInSearch: true,
     width: 30,
   },
   {
     title: '背景',
-    dataIndex: 'type',
+    dataIndex: 'bj',
     align: 'center',
     ellipsis: true,
     hideInSearch: true,
@@ -47,7 +47,7 @@ export const columns = (onEdit) => [
   },
   {
     title: '创建期',
-    dataIndex: 'time',
+    dataIndex: 'createTime',
     align: 'center',
     ellipsis: true,
     hideInSearch: true,
@@ -61,10 +61,91 @@ export const columns = (onEdit) => [
     hideInSearch: true,
     render: () => (
       <div>
-        <a type="link" size="small" onClick={() => onEdit()}>
+        <a type="link" size="small">
           编辑
         </a>
       </div>
     ),
   },
 ];
+
+export const schema = ({ queryData }) => {
+  return {
+    type: 'object',
+    displayType: 'row',
+    labelWidth: '100%',
+    properties: {
+      platformName: {
+        title: '称谓',
+        type: 'string',
+        widget: 'input',
+        placeholder: '请输入称谓',
+        required: true,
+        defaultValue: queryData.platformName,
+      },
+      content: {
+        title: '等级权重',
+        type: 'string',
+        widget: 'select',
+        placeholder: '请输入等级权重',
+        required: true,
+        defaultValue: queryData.content,
+        props: {
+          options: [
+            { label: '1', value: 'a' },
+            { label: '2', value: 'b' },
+            { label: '3', value: 'c' },
+            { label: '4', value: 'd' },
+            { label: '5', value: 'e' },
+            { label: '6', value: 'f' },
+          ],
+        },
+      },
+      input1: {
+        title: '等级外观',
+        type: 'string',
+        widget: 'input',
+        placeholder: '请输入等级外观',
+        required: true,
+        defaultValue: queryData.input1,
+      },
+      input2: {
+        title: '等级描述',
+        type: 'string',
+        widget: 'input',
+        placeholder: '请输入等级外观',
+        required: true,
+        defaultValue: queryData.input2,
+      },
+      input3: {
+        title: '升级条件',
+        type: 'string',
+        widget: 'input',
+        placeholder: '请输入升级条件',
+        required: true,
+        defaultValue: queryData.input3,
+      },
+      input4: {
+        title: '权益设置',
+        type: 'string',
+        widget: 'input',
+        placeholder: '请输入权益设置',
+        defaultValue: queryData.input4,
+      },
+      input5: {
+        title: '升级权益',
+        type: 'string',
+        widget: 'input',
+        placeholder: '请输入升级权益',
+        defaultValue: queryData.input5,
+      },
+      input6: {
+        title: '会员有效期',
+        type: 'string',
+        widget: 'input',
+        placeholder: '请输入会员有效期:',
+        defaultValue: queryData.input6,
+      },
+    },
+  };
+};
