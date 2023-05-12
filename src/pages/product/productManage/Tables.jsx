4a5ea04db10281ff6f5aeadf44f832bbe8a78612
WX <42308734@qq.com>
2023-05-12 18:32:29 +0800

    fix: 修复商品管理tab无法切换

--- src/pages/product/productManage/Tables.jsx
+++ src/pages/product/productManage/Tables.jsx
@@ -164,7 +164,7 @@ export default function Tables() {
             dispatch({
               type: 'productManage/update',
               payload: {
-                select: { activeKey: key },
+                activeKey: key,
               },
             });
             ref?.current?.reload();
