79e3f1777a95ed627f7cd945829fe16bd4b2c9c2
shenzhiqing <shenzhiqing@nihaosi.com>
2023-05-12 18:29:37 +0800

    fix(group): 修改分组管理页面增加分组管理增删改查接口调用

--- src/pages/product/groupManage/EditForm/index.jsx
+++ src/pages/product/groupManage/EditForm/index.jsx
@@ -1,33 +1,60 @@
 import { ButtonGroupPro } from '@antdp/antdp-ui';
-import { useModel } from '@umijs/max';
-import { Modal } from 'antd';
+import { Cascader, Modal } from 'antd';
 import FormRender, { useForm } from 'form-render';
-import { useEffect } from 'react';
+import { connect } from 'umi';
 
-export default () => {
+const EditForm = (props) => {
+  const { dispatch, groupManage, loading } = props;
+  const { addOpen } = groupManage;
+
   const form = useForm();
-  const {
-    store: { visible, queryInfo },
-    update,
-  } = useModel('groupManage', (model) => ({
-    ...model,
-  }));
+
+  const update = (data) => {
+    dispatch({
+      type: 'groupManage/updateState',
+      payload: data,
+    });
+  };
 
   const schema = {
     type: 'object',
     properties: {
+      location: {
+        title: '上级分类',
+        type: 'string',
+        widget: 'cascader',
+        required: true,
+        props: {
+          expandTrigger: 'hover',
+          options: options,
+          changeOnSelect: true,
+        },
+      },
+
       input: {
         title: '分组名称',
         type: 'string',
         required: true,
         props: {},
       },
+      radio1: {
+        title: '是否显示',
+        required: true,
+        type: 'string',
+        widget: 'radio',
+        props: {
+          options: [
+            { label: '是', value: 'a' },
+            { label: '否', value: 'b' },
+          ],
+        },
+      },
     },
   };
 
-  useEffect(() => {
-    form.setValues({ input: queryInfo.input });
-  }, [visible, queryInfo]);
+  // useEffect(() => {
+  //   form.setValues({ input: drawerParams.input });
+  // }, [addOpen, drawerParams]);
 
   const onFinish = (data) => {
     console.log('data', data);
@@ -36,26 +63,74 @@ export default () => {
   return (
     <Modal
       title="分组信息"
-      open={visible}
-      onCancel={() => update({ visible: false })}
+      open={addOpen}
+      onCancel={() => update({ addOpen: false })}
       footer={
         <ButtonGroupPro
           button={[
             {
               type: 'primary',
               label: '确认',
+              loading: loading,
               onClick: form.submit,
             },
             {
               type: 'primary',
               label: '取消',
-              onClick: () => update({ visible: false }),
+              onClick: () => update({ addOpen: false }),
             },
           ]}
         />
       }
     >
-      <FormRender form={form} schema={schema} onFinish={onFinish} />
+      <FormRender
+        widgets={{ cascader: Cascader }}
+        form={form}
+        schema={schema}
+        onFinish={onFinish}
+      />
     </Modal>
   );
 };
+
+export default connect(({ groupManage, loading }) => {
+  return {
+    groupManage,
+    loading: loading.effects['groupManage/selectPage'],
+  };
+})(EditForm);
+
+const options = [
+  {
+    value: 'zhejiang',
+    label: 'Zhejiang',
+    children: [
+      {
+        value: 'hangzhou',
+        label: 'Hanzhou',
+        children: [
+          {
+            value: 'xihu',
+            label: 'West Lake',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    value: 'jiangsu',
+    label: 'Jiangsu',
+    children: [
+      {
+        value: 'nanjing',
+        label: 'Nanjing',
+        children: [
+          {
+            value: 'zhonghuamen',
+            label: 'Zhong Hua Men',
+          },
+        ],
+      },
+    ],
+  },
+];
--- src/pages/product/groupManage/index.jsx
+++ src/pages/product/groupManage/index.jsx
@@ -1,28 +1,51 @@
-import { selectPage } from '@/service/memberManage';
 import { ProTable } from '@ant-design/pro-components';
 import { ButtonGroupPro } from '@antdp/antdp-ui';
-import { history, useModel } from '@umijs/max';
+import { history } from '@umijs/max';
 import { Modal } from 'antd';
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'umi';
 import EditForm from './EditForm';
 import { columns } from './columns';
 
-export default function SearchTable() {
-  const [pageSize, setPageSize] = useState(10);
-  const { update } = useModel('groupManage', (model) => ({ ...model }));
+const SearchTable = (props) => {
+  const { dispatch, groupManage, loading } = props;
+  const { pageSize } = groupManage;
+
+  useEffect(() => {
+    dispatch({
+      type: 'groupManage/getAllCategory',
+    });
+  }, []);
+
+  const update = (data) => {
+    dispatch({
+      type: 'groupManage/updateState',
+      payload: data,
+    });
+  };
+
+  const selectPage = (pg, pgz) => {
+    dispatch({
+      type: 'groupManage/selectPage',
+      payload: {
+        page: pg,
+        pageSize: pgz,
+      },
+    });
+  };
 
   const handleEdit = (type) => {
     update({ type });
     if (type === 'add') {
       update({
-        queryInfo: {},
-        visible: true,
+        drawerParams: {},
+        addOpen: true,
       });
     }
     if (type === 'edit') {
       update({
-        queryInfo: { input: '测试编辑' },
-        visible: true,
+        drawerParams: { input: '测试编辑' },
+        addOpen: true,
       });
     }
     if (type === 'manage') {
@@ -39,25 +62,17 @@ export default function SearchTable() {
   return (
     <React.Fragment>
       <ProTable
+        loading={loading}
         options={false}
         request={async (params = {}) => {
+          console.log('params: ', params);
           const { current, pageSize, ...formData } = params;
-          const { code, data } = await selectPage({
-            current,
-            pageSize,
-            queryData: { ...formData },
-          });
-          if (code === 1) {
-            return {
-              data: data.rows || [],
-              total: data.total,
-              success: true,
-            };
-          }
+          update({ page: current, pageSize, searchParams: formData });
+          selectPage(current, pageSize);
         }}
         pagination={{
           pageSize: pageSize,
-          onChange: (_, pageSize) => setPageSize(pageSize),
+          onChange: (_, pageSize) => update({ pageSize }),
           showSizeChanger: true,
         }}
         cardBordered
@@ -81,4 +96,11 @@ export default function SearchTable() {
       <EditForm />
     </React.Fragment>
   );
-}
+};
+
+export default connect(({ groupManage, loading }) => {
+  return {
+    groupManage,
+    loading: loading.effects['groupManage/selectPage'],
+  };
+})(SearchTable);
