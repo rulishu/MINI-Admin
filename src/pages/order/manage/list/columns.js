b0df4447960450f46d44d0926e0508f3515b03f2
rulishu <1324214367@qq.com>
2023-05-13 15:07:22 +0800

    fix: 订单列表调用分页和查询详情接口

--- src/pages/order/manage/list/columns.js
+++ src/pages/order/manage/list/columns.js
@@ -7,31 +7,31 @@ export const columns = (handle) => [
   },
   {
     title: '公司名称',
-    dataIndex: 'id',
+    dataIndex: 'companyName',
     align: 'center',
     hideInSearch: true,
   },
   {
     title: '订单编号',
-    dataIndex: 'id',
+    dataIndex: 'orderNumber',
     align: 'center',
   },
   {
     title: '物流单号',
-    dataIndex: 'id',
+    dataIndex: 'outOrderNo',
     align: 'center',
     hideInSearch: true,
   },
   {
     title: '下单日期',
-    dataIndex: 'id',
+    dataIndex: 'createTime',
     align: 'center',
     valueType: 'date',
     // sorter: true,
   },
   {
     title: '收货人',
-    dataIndex: 'id',
+    dataIndex: 'consignee',
     align: 'center',
   },
   {
@@ -42,13 +42,13 @@ export const columns = (handle) => [
   },
   {
     title: '订单金额',
-    dataIndex: 'id',
+    dataIndex: 'orderPrice',
     align: 'center',
     hideInSearch: true,
   },
   {
     title: '订单状态',
-    dataIndex: 'id',
+    dataIndex: 'orderStatus',
     align: 'center',
     valueType: 'select',
     valueEnum: {
@@ -61,7 +61,7 @@ export const columns = (handle) => [
   },
   {
     title: '开票状态',
-    dataIndex: 'id',
+    dataIndex: 'isOpenInvoice',
     align: 'center',
     valueType: 'select',
     valueEnum: {
@@ -87,26 +87,157 @@ export const columns = (handle) => [
 ];
 
 export const schema = ({ queryData }) => {
+  const itemList = queryData?.itemList || [];
   return {
     type: 'object',
     displayType: 'row',
     labelWidth: '100%',
     properties: {
-      platformName: {
-        title: '权益规则名称',
+      orderNumber: {
+        title: '订单编号',
         type: 'string',
-        widget: 'input',
-        placeholder: '请输入权益规则名称',
-        required: true,
-        defaultValue: queryData.platformName,
+        span: 12,
+        defaultValue: queryData.orderNumber,
       },
-      content: {
-        title: '权益规则详情',
+      userName: {
+        title: '卖家信息',
         type: 'string',
-        widget: 'textArea',
-        placeholder: '请输入权益规则详情',
-        required: true,
-        defaultValue: queryData.content,
+        span: 12,
+        defaultValue: queryData.userName,
+      },
+      phone: {
+        title: '手机号',
+        type: 'string',
+        span: 12,
+        defaultValue: queryData.phone,
+      },
+      consignee: {
+        title: '买家信息',
+        type: 'string',
+        span: 12,
+        defaultValue: queryData.consignee,
+      },
+
+      itemList: {
+        title: '商品详情',
+        type: 'array',
+        widget: 'tableList',
+        items: {
+          type: 'object',
+          properties: {
+            mainGraph: {
+              title: '商品图片',
+              type: 'string',
+            },
+            brandName: {
+              title: '商品名称',
+              type: 'string',
+              defaultValue: itemList?.at(0)?.categoryName || '',
+            },
+            input3: {
+              title: '规格',
+              type: 'string',
+              defaultValue: itemList?.at(0)?.brandName || '',
+            },
+            input4: {
+              title: '型号',
+              type: 'string',
+              defaultValue: itemList?.at(0)?.brandName || '',
+            },
+            input5: {
+              title: '数量',
+              type: 'string',
+              defaultValue: itemList?.at(0)?.brandName || '',
+            },
+            input6: {
+              title: '商品价格(元)',
+              type: 'string',
+              defaultValue: itemList?.at(0)?.unitPrice || '',
+            },
+            input7: {
+              title: '运费',
+              type: 'string',
+              defaultValue: itemList?.at(0)?.brandName || '',
+            },
+          },
+        },
+      },
+
+      list: {
+        type: 'array',
+        widget: 'simpleList',
+        display: 'inline',
+        props: {
+          hasBackground: true,
+        },
+        items: {
+          type: 'object',
+          properties: {
+            createTime: {
+              title: '下单时间',
+              type: 'string',
+              defaultValue: queryData.createTime,
+            },
+            orderStatus: {
+              title: '订单状态',
+              type: 'string',
+              defaultValue: queryData.orderStatus,
+            },
+          },
+        },
+      },
+      list1: {
+        type: 'array',
+        widget: 'simpleList',
+        display: 'inline',
+        props: {
+          hasBackground: true,
+        },
+        items: {
+          type: 'object',
+          displayType: 'row',
+          properties: {
+            consignee: {
+              title: '收货人',
+              type: 'string',
+              defaultValue: queryData.consignee,
+              span: 12,
+            },
+            phone: {
+              title: '手机号',
+              type: 'string',
+              defaultValue: queryData.phone,
+              span: 12,
+            },
+            address: {
+              title: '收货地址',
+              type: 'string',
+              defaultValue: queryData.address,
+              span: 12,
+              labelCol: 7,
+              fieldCol: 16,
+            },
+            orderNumber: {
+              title: '快递单号',
+              type: 'string',
+              defaultValue: queryData.orderNumber,
+              span: 12,
+              labelCol: 7,
+              fieldCol: 16,
+            },
+            companyName: {
+              title: '物流公司',
+              type: 'string',
+              span: 24,
+              defaultValue: queryData.companyName,
+            },
+            isOpenInvoice: {
+              title: '开票状态',
+              type: 'string',
+              defaultValue: queryData.isOpenInvoice,
+            },
+          },
+        },
       },
     },
   };
