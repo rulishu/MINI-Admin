93b1a7d7a1b987c256fa93f4b264bd0124d8bfbb
zheng <zhouzheng0646@qq.com>
2023-05-22 16:37:13 +0800

    fix: 会员分润分润界面调整

--- src/pages/profitSharing/members/index.jsx
+++ src/pages/profitSharing/members/index.jsx
@@ -3,9 +3,6 @@ import Table from './Table';
 const members = () => {
   return (
     <div>
-      <div style={{ marginBottom: '15px', marginTop: '10px' }}>
-        <p>说明：会员分润基数为实际毛利</p>
-      </div>
       <Table />
     </div>
   );
